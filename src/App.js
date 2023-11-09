e7034929e22e01cac4f0753d5e6a64e18e07db7b
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-11-09 19:39:32 +1100

    add in income and expenses component UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
         <Header />
         <div className="container">
             <Balance />
+            <IncomeExpenses />
         </div>
     </div>
   );
