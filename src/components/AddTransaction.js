0e261aea99370ee6497c3fe52db8b2984a9241d9
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-11-09 19:49:19 +1100

    add in transaction form component UI

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const AddTransaction = () => {
+  return (
+    <>
+        <h3>Add new transaction</h3>
+        <form >
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" placeholder="Enter text..." />
+        </div>
+        <div class="form-control">
+            <label for="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+            >
+            <input type="number" placeholder="Enter amount..." />
+        </div>
+        <button class="btn">Add transaction</button>
+      </form>
+    </>
+  )
+}
